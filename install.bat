71969b808b47e0caca91ae0b7bb18a84ed975f3d
forumvmatr67-oss <forumvmatr67@gmail.com>
2026-05-20 16:05:57 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,66 +1,53 @@
 @echo off
-title GBS Installer
-color 0A
-
 echo ====================================
 echo      GBS Language Installer
 echo ====================================
 echo.
 
-:: Создание папок
-if not exist "src" mkdir src
-if not exist "examples" mkdir examples
 if not exist "scripts" mkdir scripts
+if not exist "examples" mkdir examples
 
-:: Копирование интерпретатора
-copy nul gbs.bat >nul
+echo @echo off > gbs.bat
+echo setlocal enabledelayedexpansion >> gbs.bat
+echo if "%%1"=="" ( >> gbs.bat
+echo   echo Usage: gbs script.gbs >> gbs.bat
+echo   exit /b 1 >> gbs.bat
+echo ) >> gbs.bat
+echo if not exist "%%1" ( >> gbs.bat
+echo   echo File not found! >> gbs.bat
+echo   exit /b 1 >> gbs.bat
+echo ) >> gbs.bat
+echo for /f "usebackq delims=" %%%%a in ("%%1") do ( >> gbs.bat
+echo   set "line=%%%%a" >> gbs.bat
+echo   if "!line:~0,5!"=="print" ( >> gbs.bat
+echo     echo !line:~6! >> gbs.bat
+echo   ) >> gbs.bat
+echo   if "!line:~0,3!"=="set" ( >> gbs.bat
+echo     echo Setting variable... >> gbs.bat
+echo   ) >> gbs.bat
+echo   if "!line!"=="io.read()" ( >> gbs.bat
+echo     set /p input="^> " >> gbs.bat
+echo   ) >> gbs.bat
+echo ) >> gbs.bat
 
-:: Создание тестового скрипта в examples
-(
-echo # Example GBS Script
-echo print "Hello from GBS!"
-echo print "================="
-echo set name = "GBS User"
-echo print "Welcome, "
-echo print name
-echo.
-echo print "Enter your name:"
-echo io.read()
-echo print "Hello, "
-echo print last_input
-) > examples\test.gbs
+echo @echo off > run.bat
+echo call gbs.bat %%1 >> run.bat
+echo pause >> run.bat
 
-:: Создание демо скрипта
-(
-echo # Demo Script
-echo print "=== GBS Demo ==="
-echo set x = 10
-echo set y = 20
-echo set sum = x + y
-echo print "Sum: "
-echo print sum
-echo.
-echo if x ^< y
-echo     print "x is less than y"
-echo endif
-echo.
-echo for i = 1 to 3
-echo     print "Count: "
-echo     print i
-echo end
-) > scripts\demo.gbs
+echo print Hello World! > examples\hello.gbs
+echo print This is GBS Language >> examples\hello.gbs
+echo print ===================== >> examples\hello.gbs
+echo io.read() >> examples\hello.gbs
 
 echo.
 echo ====================================
-echo GBS successfully installed!
+echo GBS Installed!
 echo ====================================
 echo.
 echo Commands:
-echo   gbs script.gbs     - run script
-echo   find               - find all .gbs files
-echo   new name           - create new script
-echo   run script.gbs     - run with pause
+echo   gbs script.gbs
+echo   run script.gbs
 echo.
-echo Examples in 'examples\' and 'scripts\'
+echo Example: gbs examples\hello.gbs
 echo.
 pause
